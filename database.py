e54d6ff6e35e70ee09188afe1826437174c0b6c2
Barani <bannu0587@gmail.com>
2026-06-20 11:27:22 +0530

    Added category field

--- database.py
+++ database.py
@@ -5,7 +5,16 @@ db = SQLAlchemy()
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
-    price = db.Column(db.String(50))
-    image = db.Column(db.String(300))
-    affiliate_link = db.Column(db.String(500))
+
+    title = db.Column(db.String(200), nullable=False)
+
+    price = db.Column(db.String(50), nullable=False)
+
+    category = db.Column(db.String(50), nullable=False)
+
+    image = db.Column(db.String(300), nullable=False)
+
+    affiliate_link = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return f"<Product {self.title}>"
